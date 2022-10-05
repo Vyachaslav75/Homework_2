804b98119a9a2f5225b0d77ffb1d6a1a3e333f61
Vyachaslav75 <kslava@tut.by>
2022-10-05 22:22:48 +0300

    Add Task 10

---
+++ Task_10/Task_10.cs
@@ -0,0 +1,20 @@
+﻿int enterNumber()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите целое трехзначное число: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            if (number > 99 && number < 1000)
+            {
+                return number;
+            }
+
+        }
+        Console.WriteLine("Введено неправильное число");
+    }
+}
+Console.Clear();
+int userNumber = enterNumber();
+Console.WriteLine($"{userNumber} -> {(userNumber % 100) / 10}");
