c67d4fce9e80bd5e560f319d134440a17c1bfda4
Vyachaslav75 <kslava@tut.by>
2022-10-05 22:37:46 +0300

    Add Second variant Task 13

---
+++ Task_13_1/Task_13_1.cs
@@ -0,0 +1,31 @@
+﻿int enterNumber()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            return number;
+        }
+        Console.WriteLine("Введено неправильное число");
+    }
+}
+Console.Clear();
+int userNumber = enterNumber();
+int tmp = userNumber;
+int count = 1;
+while (userNumber / 10 != 0)
+{
+    userNumber = userNumber / 10;
+    count = count * 10;
+}
+if (tmp < 100)
+{
+    Console.WriteLine($"{tmp} -> нет третьей цифры");
+}
+else
+{
+    count = (tmp / (count / 100)) % 10;
+    Console.WriteLine($"{tmp} -> {count}");
+}
