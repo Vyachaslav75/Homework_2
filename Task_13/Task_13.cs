162b8b4ce5ca595adb28dfdc79e91c2e562c7de6
Vyachaslav75 <kslava@tut.by>
2022-10-05 22:26:44 +0300

    Add Task 13

---
+++ Task_13/Task_13.cs
@@ -0,0 +1,24 @@
+﻿string enterNumberStr()
+{
+    string numberStr;
+    while (true)
+    {
+        Console.Write("Введите целое число: ");
+        numberStr = Console.ReadLine() ?? "";
+        if (int.TryParse(numberStr, out int a2))
+        {
+            return numberStr;
+        }
+        Console.WriteLine("Ввод неверный");
+    }
+}
+Console.Clear();
+string str = enterNumberStr();
+if (str.Length > 2)
+{
+    Console.WriteLine($"{str} -> {str[2]}");
+}
+else
+{
+    Console.WriteLine($"{str} -> третьей цифры нет");
+}
