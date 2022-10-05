e62de463ba470650caecf4832c442cafe82d3861
Vyachaslav75 <kslava@tut.by>
2022-10-05 22:30:18 +0300

    Add Task 15

---
+++ Task_15/Task_15.cs
@@ -0,0 +1,23 @@
+﻿int enterNumberWeek()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите номер дня недели: ");
+        if (int.TryParse(Console.ReadLine(), out number) && (number > 0 && number < 8))
+        {
+            return number;
+        }
+        Console.WriteLine("Введите число от 1 до 7");
+    }
+}
+Console.Clear();
+int dayNumber = enterNumberWeek();
+if (dayNumber > 5)
+{
+    Console.WriteLine($"{dayNumber} -> да");
+}
+else
+{
+    Console.WriteLine($"{dayNumber} -> нет");
+}
